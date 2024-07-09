b632c0d5c9c47d2e1f2d49123d43f24ea9aab923
MatheusMelloDev <matehuskbfbv@gmail.com>
2024-07-09 13:43:51 -0300

    estruturei e inicializei o projeto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom';
+import Login from './screens/login';
+import './App.css'
+
+function App() {
+
+
+  return (
+  <BrowserRouter>
+    <Login />
+    </BrowserRouter>
+  )
+}
+
+export default App
